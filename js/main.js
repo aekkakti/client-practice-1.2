838bf1486e2889fc03c7d1e20fd629d4f900d8e3
aekkakti <music.tomsk1@gmail.com>
2024-02-01 16:05:09 +0700

    Попытка заполнения TODO

--- js/main.js
+++ js/main.js
@@ -1,7 +1,38 @@
+Vue.component('column-1', {
+    template: `
+        
+    <div>
+    <h1>Столбец 1</h1>
+    <h2>Добавить название</h2>
+        <input type="text" name="card">
+        <button @click="addCard">Add</button>
+     </div>
+    `,
+    data() {
+        return {
+            cards: []
+        }
+    },
+    methods: {
+        addList(value) {
+            this.$emit('change', value)
+        },
+    }
+})
 
+// Проверка работоспособности LocalStorage
 let app = new Vue({
     el: '#todo',
     data: {
-        title: 'Hello'
+        name:'',
+        column1Cards: [],
+        column2Cards: [],
+        column3Cards: []
+    },
+    methods: {
+        addCard(value) {
+            this.cards.push(this.card)
+            console.log(this.cards);
+        }
     }
 })
