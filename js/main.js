67ae0dfca7988488c2308f99d47f424c87fc9df3
aekkakti <music.tomsk1@gmail.com>
2024-02-02 11:08:31 +0700

    Выдаёт ошибку: (Property or method "columns" is not defined on the instance but referenced during render. Make sure that this property is reactive, either in the data option, or for class-based components, by initializing the property.) Необходимо пофиксить

--- js/main.js
+++ js/main.js
@@ -1,37 +1,65 @@
 Vue.component('column', {
-    props: ['cards', 'addCard', 'title'],
     template: `
-        <div>
-            <h1>{{ title }}</h1>
-            <h2>Добавить название</h2>
-            <input type="text" v-model="newCard" name="card">
-            <button @click="addCard(newCard)">Add</button>
+        <div class="t-a-c">
+            <h1 class="fs-32pt">{{ title }}</h1><br>
+            <h2>Добавить карточку</h2>
+            <input type="text" v-model="name"><br><br>
+            <button @click="addCard(name)">Добавить</button>
             <ul>
-                <li v-for="card in cards">{{ card }}</li>
+                <li v-for="(card, index) in cards">
+                <p>{{ card.name }}</p>           
+                <add-tasks :tasks="card.tasks" @add-task="addTask(index, $event)"></add-tasks>
+                </li>
             </ul>
+            
         </div>
     `,
+    methods: {
+        addCard() {
+            if (this.count < 3){
+                this.cards.push({name: this.name, tasks: []})
+                this.name = '';
+                this.count += 1;
+            }
+        },
+        addTask(cardIndex, newTask) {
+            this.cards[cardIndex].tasks.push(newTask)
+        }
+    },
     data() {
         return {
-            newCard: ''
+            name: '',
+            cards: [],
+            count: 0,
         }
     },
 });
 
-let app = new Vue({
-    el: '#todo',
-    data: {
-        columns: {
-            column1: { title: 'Принято в работу', cards: [] },
-            column2: { title: 'Выполняется', cards: [] },
-            column3: { title: 'Готово', cards: [] }
+Vue.component('add-tasks', {
+    props: ['tasks'],
+    template: `
+    <ul>
+        <li v-for="(task, index) in tasks" :key="index">
+            <p>{{task.name}}</p>
+        </li>
+        <h2>Добавить задачу</h2>
+        <input type="text" v-model="newTask" name="task">
+        <button @click="addTask">Добавить задачу</button>
+    </ul>
+    `,
+    data() {
+        return {
+            newTask: ''
         }
     },
     methods: {
-        addCard(column) {
-            return (newCard) => {
-                this.columns[column].cards.push(newCard);
-            };
-        }
+        addTask() {
+            this.$emit('add-task', {name: this.newTask})
+            this.newTask = ''
+        },
     }
+})
+
+let app = new Vue({
+    el: '#todo',
 });
